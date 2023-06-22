90b4b3209f0a64e59199423836ba91d6c0e41feb
Always Distracted <padhmanabbanbascarane@gmail.com>
2023-06-22 18:17:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top= window.scrollY;
+        let offset= sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top>=offset && top< offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav [href*='+ id +']').classList.add('active');
+            });
+        };
+
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.tools-container, .container-box, #contact, .about-me', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img, .title, .skills-content', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content, ', {origin: 'right'});
+
+
